f828ecc5c2cd57f664691e655abc72e2021c8bd6
Shashlyk <hi@ip.ru>
2025-12-19 19:40:20 +0300

    Fixed

--- src/structures/stack_list.py
+++ src/structures/stack_list.py
@@ -1,48 +1,51 @@
-from src.sorts.heap_min import HeapMin
 class Stack:
-    def __init__(self, max_size):
+    def __init__(self, max_size: int):
         if max_size <= 0:
             raise ValueError("Stack size must be positive")
-        self.body = [0] * max_size
-        self.i_ptr = -1
+        self.body = []
+        self.min_els = []   # массив минимумов
         self.max_size = max_size
+        
 
     def pop(self) -> int:
         if self.is_empty():
             raise IndexError("Error: Stack is empty\n")
-
-        popped_el = self.body[self.i_ptr]
-        self.i_ptr -= 1
-        return popped_el
+        
+        self.min_els.pop()
+        
+        return self.body.pop()
 
     def push(self, x: int) -> None:
         if self.is_full():
             raise IndexError("Error: Stack overflow\n")
 
-        self.i_ptr += 1
-        self.body[self.i_ptr] = x
+        
+        if not self.is_empty():
+            current_min = min(x, self.min_els[-1])
+        else:
+            current_min = x
+        self.min_els.append(current_min)
+
+        self.body.append(x)
 
     def peek(self) -> int:
         if self.is_empty():
             raise IndexError("Error: Stack is empty\n")
 
-        return self.body[self.i_ptr]
+        return self.body[-1]
 
     def is_empty(self) -> bool:
-        return self.i_ptr == -1
+        return len(self.body) == 0
 
     def is_full(self) -> bool:
-        return self.i_ptr + 1 >= self.max_size
+        return len(self.body) >= self.max_size
 
-    # O(n)
+    # O(1)
     def min(self) -> int:
         if self.is_empty():
             raise IndexError("Error: Stack is empty")
 
-        # Брать только заполненную часть стэка
-        stack_elements = self.body[:self.i_ptr + 1]
-        heap = HeapMin.build_heap(stack_elements)
-        return heap.min()
+        return self.min_els[-1]
 
     def __len__(self) -> int:
-        return self.i_ptr + 1
+        return len(self.body)
